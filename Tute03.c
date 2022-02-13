c027793b6a05cd175a68b19073f33a558a51d498
Chamath Anuradha <87602649+IT21044304@users.noreply.github.com>
2022-02-13 10:36:22 +0000

    Tutorial 3 - Done all the coding to calculate the sum of the numbers from 1 to n.

--- Tute03.c
+++ Tute03.c
@@ -13,7 +13,20 @@ sum = 1+2+3+...+10 = 55 */
 
 #include <stdio.h>
 int main() {
-  
+
+  int sum = 0, i, num;
+
+  printf("Enter the your Number : ");
+  scanf("%d", &num);
+
+  for(i=1; i<num; i++)
+  {
+    sum = sum + num;
+    i++;
+  }
+
+  printf("The sum of the Numbers is : %d ", sum);
+
   return 0;
 }
 
