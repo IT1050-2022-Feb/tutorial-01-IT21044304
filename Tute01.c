ff524088f34c5d01d179019d3902455514299e79
Chamath Anuradha <87602649+IT21044304@users.noreply.github.com>
2022-02-13 07:49:28 +0000

    tutorial 1 - Typed the Code to Entered 2 marks as input and get the average of those two marks.

--- Tute01.c
+++ Tute01.c
@@ -5,7 +5,19 @@
 #include <stdio.h>
 
 int main() {
-  
+   
+   int m1, m2, Avg;
+
+   printf("Enter Marks for subject 1 : ");
+   scanf("%d", &m1);
+   
+   printf("Enter Marks for subject 2 : ");
+   scanf("%d", &m2);
+
+   Avg = (m1 + m2)/2;
+
+   printf("Average marks of the two subjects is : %d ", Avg);
+   
   return 0;
 }
 
